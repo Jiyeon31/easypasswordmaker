101c62f875886ad67aae084f52e79d2cc4e81f4c
Jiyeon31 <zhdzhddlx@gmail.com>
2022-01-09 22:40:18 -0500

    moved files to a folder to connect to git repository

---
+++ assets/js/script.js
@@ -0,0 +1,64 @@
+// Assignment code here
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+var password = document.getElementById("password");
+
+const keys = {
+  upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  lowerCase: "abcdefghijklmnopqrstuvwxyz",
+  number: "0123456789",
+  symbol: "!@#$%^&*()_+~\\`|}{[]:;?><,./-="
+}
+const getKey = [
+  function upperCase() {
+    return keys.upperCase[Math.floor(Math.random() * keys.upperCase.length)];
+  },
+  function lowerCase() {
+    return keys.lowerCase[Math.floor(Math.random() * keys.lowerCase.length)];
+  },
+  function number() {
+    return keys.number[Math.floor(Math.random() * keys.number.length)];
+  },
+  function symbol() {
+    return keys.symbol[Math.floor(Math.random() * keys.symbol.length)];
+  }
+];
+
+function ShowAndHide() {
+    var x = document.getElementById('option');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function generatePassword() {
+  const upper = document.getElementById("upperCase").checked;
+  const lower = document.getElementById("lowerCase").checked;
+  const number = document.getElementById("number").checked;
+  const symbol = document.getElementById("symbol").checked;
+  if (upper + lower + number + symbol === 0) {
+    alert("Please check at least one box!");
+    return;
+  }
+  const generate = document.getElementById("generate");
+  const length = document.getElementById("length");
+  let password = "";
+  while (length.value > password.length) {
+    let keyToAdd = getKey[Math.floor(Math.random() * getKey.length)];
+    let isChecked = document.getElementById(keyToAdd.name).checked;
+    if (isChecked) {
+      password += keyToAdd();
+    }
+  }
+  generate.innerHTML = password;
+};
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", generatePassword);
+
